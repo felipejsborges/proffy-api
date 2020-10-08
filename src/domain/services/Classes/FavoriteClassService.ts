7ce4c76f7be7c75e6e3a048608c8daec58b7d5d1
Felipe Borges <felipejsborges@outlook.com>
2020-10-07 21:43:17 -0300

    favorite classes bug fixed

--- src/domain/services/Classes/FavoriteClassService.ts
+++ src/domain/services/Classes/FavoriteClassService.ts
@@ -9,6 +9,18 @@ class FavoriteClassService {
 	constructor(private favoritesRepository: iFavoritedClassesRepository) {}
 
 	public async execute({ user_id, class_id }: Request): Promise<void> {
+		const favoritedClassesByThisUser = await this.favoritesRepository.findAllByUserId(
+			user_id,
+		);
+
+		const checkIfExist = favoritedClassesByThisUser.find(
+			favoritedClass => favoritedClass.class_id === class_id,
+		);
+
+		if (checkIfExist) {
+			return;
+		}
+
 		await this.favoritesRepository.save({
 			user_id,
 			class_id,
